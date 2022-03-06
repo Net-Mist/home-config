9548467eac0d032088fb5c00f81eb9969ba773f1
Sebastien Iooss <archimist.linux@gmail.com>
2022-03-06 15:41:26 +0100

    austin adn clickhouse-sqlalchemy ready to be in nixpkgs

--- python/clickhouse-sqlalchemy.nix
+++ python/clickhouse-sqlalchemy.nix
@@ -1,18 +1,24 @@
-# with import <nixpkgs> {};
-{ buildPythonPackage
-, fetchPypi
+{ lib
+, stdenv
+, buildPythonPackage
+, fetchFromGitHub
 , sqlalchemy
 , clickhouse-driver
 , requests
+, nose
+, parameterized
+, responses
 }:
 
 buildPythonPackage rec {
   pname = "clickhouse-sqlalchemy";
   version = "0.2.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-nA5ECcH0gKJFdZcG0pdxFErtFy15YSLZioAsMT25nGM";
+  src = fetchFromGitHub {
+    owner = "xzkostyan";
+    repo = "clickhouse-sqlalchemy";
+    rev = version;
+    hash = "sha256-lkRSc2vREL67+wNY4hSaZef2/TeNgdu4sdYiv/rPTvw";
   };
 
   propagatedBuildInputs = [
@@ -21,5 +27,20 @@ buildPythonPackage rec {
     requests
   ];
 
+  checkInputs = [
+    nose
+    parameterized
+    responses
+  ];
+
+  # tests require a clickhouse database running in localhost.
+  # see docker-compose file in tests dir
   doCheck = false;
+
+  meta = with lib; {
+    description = "ClickHouse dialect for SQLAlchemy ";
+    homepage = "https://github.com/xzkostyan/clickhouse-sqlalchemy";
+    license = licenses.mit;
+    maintainers = with maintainers; [ net-mist ];
+  };
 }
